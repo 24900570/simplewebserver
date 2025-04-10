dcd6415360524d895ca52ea3cfdd20e5bbe76deb
24900570 <ajithanbu2910@gmail.com>
2025-04-10 23:33:49 +0530

    Success

--- webnew.py
+++ webnew.py
@@ -1,31 +1,3 @@
-# EX01 Developing a Simple Webserver
-## Date:10.04.2025
-## NAME:Ajith A
-## REG NO:212224230012
-
-
-## AIM:
-To develop a simple webserver to serve html pages and display the list of protocols in TCP/IP Protocol Suite.
-
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation.
-
-### Step 2:
-Design of webserver workflow.
-
-### Step 3:
-Implementation using Python code.
-
-### Step 4:
-Serving the HTML pages.
-
-### Step 5:
-Testing the webserver.
-
-## PROGRAM:
-```
 from http.server import HTTPServer,BaseHTTPRequestHandler
 
 content='''<!doctype html>
@@ -60,10 +32,3 @@ print("My webserver is running")
 server_address =('',8008)
 httpd = HTTPServer(server_address,MyServer)
 httpd.serve_forever()
-```
-## OUTPUT:
-![alt text](<Screenshot (232).png>)
-![alt text](<Screenshot (233).png>)
-
-## RESULT:
-The program for implementing simple webserver is executed successfully.
